cba20b2aa1ef66b248020d2a562f9c2c9637b22d
Ben Shurts <bshurts93@gmail.com>
2020-08-05 16:41:51 -0700

    Installed Material-UI and created navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from "react";
-import Projects from "./Projects.js";
-import Education from "./Education.js";
-import About from "./About.js";
-import { BrowserRouter, Route, Link } from "react-router-dom";
+import Projects from "./Components/Pages/Projects";
+import Education from "./Components/Pages/Education";
+import About from "./Components/Pages/About";
+import Navbar from "./Components/Navigation/Navbar";
+import { BrowserRouter, Route } from "react-router-dom";
 
 import "./App.css";
 
@@ -10,26 +11,11 @@ function App() {
   return (
     <BrowserRouter>
       <div className="App">
+        <Navbar />
         {/* Route Setup */}
         <Route exact path="/" component={Projects} />
         <Route path="/education" component={Education} />
         <Route path="/about" component={About} />
-
-        <div className="navigation">
-          {/* Replace with own logo */}
-          {/* <img src={myLogo} className="logo" alt="Ben Shurts" /> */}
-          <div className="navigation-sub">
-            <Link to="/" className="item">
-              Projects
-            </Link>
-            <Link to="/education" className="item">
-              Education
-            </Link>
-            <Link to="/about" className="item">
-              About
-            </Link>
-          </div>
-        </div>
       </div>
     </BrowserRouter>
   );
--- src/Components/Pages/Projects.js
+++ src/Components/Pages/Projects.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Thumbnail from "./Thumbnail";
+import Thumbnail from "../../Thumbnail";
 
 function Projects(props) {
   return (
