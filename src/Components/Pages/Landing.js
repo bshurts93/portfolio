1748828eedf9516bf060401e3f0f254878ba915b
Ben Shurts <bshurts93@gmail.com>
2020-08-10 17:22:05 -0700

    Playing with Framer examples

--- src/Components/Pages/Landing.js
+++ src/Components/Pages/Landing.js
@@ -1,17 +1,32 @@
 import React from "react";
 import { Container } from "@material-ui/core";
-// import { makeStyles } from "@material-ui/core/styles";
+import { Example } from "../Framer/Example";
+import { PathExample } from "../Framer/PathExample";
+import { makeStyles } from "@material-ui/core/styles";
 
-// const useStyles = makeStyles((theme) => ({}));
+const useStyles = makeStyles((theme) => ({
+  example: {
+    background: "white",
+    borderRadius: "30px",
+    width: "150px",
+    height: "150px",
+  },
+  title: {
+    marginLeft: theme.spacing(2),
+    flex: 1,
+  },
+}));
 
 export default function Landing() {
-  //   const classes = useStyles();
+  const classes = useStyles();
 
   return (
-    <div className="landing">
+    <div>
       <Container>
-        <h1>Ben Shurts</h1>
-        <h1>I like to make stuff for the web</h1>
+        <h3>Ben Shurts</h3>
+        <h3>I like to make stuff for the web</h3>
+        <Example />
+        <PathExample />
       </Container>
     </div>
   );
